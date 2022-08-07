8dfa48fcf1fdb0f4440723311a6a60c5c2e849cc
thelovekesh <lovekesh.kumar@rtcamp.com>
2022-08-07 20:45:50 +0530

    Initial Plugin Setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# WP CLS Terminator
---
+++ wp-cls-terminator.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: WP CLS Terminator
+ * Description: Terminate Layout Shift from Embeds
+ * Plugin URI:  https://rtcamp.com
+ * Author:      rtCamp
+ * Author URI:  https://rtcamp.com
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Version:     1.0
+ * Text Domain: wp-cls-terminator
+ *
+ * @package WP_CLS_Terminator
+ */
+
+define( 'WP_CLS_TERMINATOR_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
+define( 'WP_CLS_TERMINATOR_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
+
+$lps = array();
