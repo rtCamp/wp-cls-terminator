8050cb0f85d41b3d7d1e4456a46c26d483cb7c47
thelovekesh <lovekesh.kumar@rtcamp.com>
2022-08-29 16:28:22 +0530

    Fix PHPCS errors

--- includes/class-asset.php
+++ includes/class-asset.php
@@ -10,7 +10,8 @@ namespace WP_CLS_Terminator;
 /**
  * Class Assets
  */
-class Assets {
+class Asset {
+
 	/**
 	 * Init
 	 */
@@ -22,13 +23,14 @@ class Assets {
 	 * Enqueue Assets
 	 */
 	public function enqueue_assets() {
-		$deps = require(WP_CLS_TERMINATOR_PATH . '/assets/build/js/cls-terminator.asset.php');
+		$deps = require WP_CLS_TERMINATOR_PATH . '/assets/build/js/cls-terminator.asset.php';
 
 		wp_enqueue_script(
 			'wp-cls-terminator',
 			WP_CLS_TERMINATOR_URL . '/assets/build/js/cls-terminator.js',
 			$deps['dependencies'],
 			filemtime( WP_CLS_TERMINATOR_PATH . '/assets/build/js/cls-terminator.js' ),
+			true
 		);
 
 		wp_enqueue_style(
